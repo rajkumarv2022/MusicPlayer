bf0e7e32d05d5efbd81cc8fbfd25314c9b577eed
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-05-07 23:34:31 +0530

    commited

--- src/component/English.tsx
+++ src/component/English.tsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Hindi() {
+export default function English() {
+
+  //!songname || !singername || !musicdirector || !albumname || !actorname || !actressname || !releaseddate || !genrename || !language || !imageurl
+  // <img className='w-12' src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+
+   type Songdet = {
+
+      songname:string;
+      musicdirector:string;
+      albumname:string;
+      releaseddate:string;
+      genrename:string;
+      imageurl:string;
+      language:string;
+
+   };
+
+
+   const [songdetail,setSongdetails] = useState<Songdet[]>([]);
+
+
+   useEffect(() => {
+    fetch('http://localhost:3001/song/lang')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('API Response:', data);
+        const englishSongs = data.filter((song: { language: string; }) => song.language === "English");
+        setSongdetails(englishSongs);
+      })
+      .catch((error) => {
+        console.log('Error fetching data:', error);
+      });
+  }, []);
+  
+  
+
+
+
   return (
     <div className='bg-black h-full'>
 
@@ -34,28 +71,30 @@ export default function Hindi() {
 
         <div id='song' className='flex flex-col gap-4 py-4'>
 
+         {songdetail.map((song) => (
+        
           <div className='flex flex-row px-8 items-center justify-between hover:border hover:border-gray-400 rounded-xl'>
 
-            <img className='w-12' src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <img className='w-12' src={song.imageurl} alt="" />
 
             <div className='flex flex-col'>
-              <p className='text-white text-lg'>Feeling Songs</p>
-              <p className='text-gray-400'>Song</p>
+              <p className='text-white text-lg'>{song.genrename}</p>
+              <p className='text-gray-400'>{song.songname}</p>
             </div>
 
             <div className='flex flex-col'>
               <p className='text-white text-lg'>Movie Name</p>
-              <p className='text-gray-400'>Ratchasan</p>
+              <p className='text-gray-400'>{song.albumname}</p>
             </div>
 
             <div className='flex flex-col'>
               <p className='text-white text-lg'>Artist Name</p>
-              <p className='text-gray-400'>Aniruth</p>
+              <p className='text-gray-400'>{song.musicdirector}</p>
             </div>
 
             <div className='flex flex-col'>
               <p className='text-white text-lg'>Released Date</p>
-              <p className='text-gray-400'>22/04/2022</p>
+              <p className='text-gray-400'>{song.releaseddate}</p>
             </div>
 
             <div className='text-green-400'>
@@ -73,45 +112,7 @@ export default function Hindi() {
 
           </div>
 
-          <div className='flex flex-row px-8 items-center justify-between hover:border hover:border-gray-400 rounded-xl'>
-
-            <img className='w-12' src="https://images.pexels.com/photos/3680219/pexels-photo-3680219.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-
-            <div className='flex flex-col'>
-              <p className='text-white text-lg'>Feeling Songs</p>
-              <p className='text-gray-400'>Song</p>
-            </div>
-
-            <div className='flex flex-col'>
-              <p className='text-white text-lg'>Movie Name</p>
-              <p className='text-gray-400'>Ratchasan</p>
-            </div>
-
-            <div className='flex flex-col'>
-              <p className='text-white text-lg'>Artist Name</p>
-              <p className='text-gray-400'>Aniruth</p>
-            </div>
-
-            <div className='flex flex-col'>
-              <p className='text-white text-lg'>Released Date</p>
-              <p className='text-gray-400'>22/04/2022</p>
-            </div>
-
-            <div className='text-green-400'>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-play"><circle cx="12" cy="12" r="10"/><polygon points="10 8 16 12 10 16 10 8"/></svg>
-              </button>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-heart text-white"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
-              </button>
-              <span className='text-gray-400'>99</span>
-            </div>
-
-          </div>
-          
+          ))}
 
         </div>
 
@@ -120,3 +121,5 @@ export default function Hindi() {
     </div>
   )
 }
+
+
