30513fca698b1727f8ee66b2e58de5743ae5fc5a
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2024-05-07 22:26:00 +0530

    commited

--- src/Admin/AddSong.tsx
+++ src/Admin/AddSong.tsx
@@ -10,11 +10,13 @@ export default function AddSong() {
   const [actressname,setActressname]=useState('');
   const [releaseddate,setReleaseddate] = useState('');
   const [genrename,setGenrename] = useState('');
+  const [language,setLanguage] = useState('English');
+  const [imageurl,setImageurl] = useState('');
 
   const addSong = (e:any) => {
     e.preventDefault();
 
-    if (!songname || !singername || !musicdirector || !albumname || !actorname || !actressname || !releaseddate || !genrename) {
+    if (!songname || !singername || !musicdirector || !albumname || !actorname || !actressname || !releaseddate || !genrename || !language || !imageurl) {
       alert('Please fill in all fields');
       return;
     }
@@ -27,7 +29,9 @@ export default function AddSong() {
       actorname,
       actressname,
       releaseddate,
-      genrename
+      genrename,
+      language,
+      imageurl
     })
     .then(result => {
       console.log(result);
@@ -40,6 +44,9 @@ export default function AddSong() {
       setActressname('');
       setReleaseddate('');
       setGenrename('');
+      //setLanguage('');
+      setImageurl('');
+      alert('Song added successfully');
     })
     .catch(err => console.log(err));
   };
@@ -49,16 +56,25 @@ export default function AddSong() {
       <h1 className='text-3xl'>AddSong</h1>
       <form id='form1' className='flex flex-col gap-4 border border-green-400 rounded-lg h-full w-full max-w-2xl items-center justify-center text-black'>
         <div className='flex flex-col gap-8 w-full max-w-xl'>
-          <input id='inf' className='px-4 py-2 outline-green-400' value={songname} onChange={(e) => { setSongname(e.target.value) }} type="text" placeholder="Song Name"/>
-          <input className='px-4 py-2 outline-green-400' value={singername} onChange={(e) => { setSingername(e.target.value ) } } type="text" placeholder="Singer Name"/>
-          <input className='px-4 py-2 outline-green-400' value={musicdirector} onChange={(e) => { setMusicdirector(e.target.value)}} type="text" placeholder="Musicdirector Name"/>
-          <input className='px-4 py-2 outline-green-400' value={albumname} onChange={(e) => { setAlbumname(e.target.value)}} type="text" placeholder="Album Name"/>
-          <input className='px-4 py-2 outline-green-400' value={actorname} onChange={(e) => {setActorname(e.target.value)}} type="text" placeholder="Actor Name"/>
-          <input className='px-4 py-2 outline-green-400' value={actressname} onChange={(e) => {setActressname(e.target.value)}} type="text" placeholder="Actress Name"/>
-          <input className='px-4 py-2 outline-green-400' value={releaseddate} onChange={(e) => {setReleaseddate(e.target.value)}} type="text" placeholder="Released Date"/>
-          <input className='px-4 py-2 outline-green-400' value={genrename} onChange={(e) => {setGenrename(e.target.value)}} type="text" placeholder="Genre"/>
+          <input className='px-4  outline-green-400' value={songname} onChange={(e) => { setSongname(e.target.value) }} type="text" placeholder="Song Name"/>
+          <input className='px-4  outline-green-400' value={singername} onChange={(e) => { setSingername(e.target.value ) } } type="text" placeholder="Singer Name"/>
+          <input className='px-4  outline-green-400' value={musicdirector} onChange={(e) => { setMusicdirector(e.target.value)}} type="text" placeholder="Musicdirector Name"/>
+          <input className='px-4  outline-green-400' value={albumname} onChange={(e) => { setAlbumname(e.target.value)}} type="text" placeholder="Album Name"/>
+          <input className='px-4  outline-green-400' value={actorname} onChange={(e) => {setActorname(e.target.value)}} type="text" placeholder="Actor Name"/>
+          <input className='px-4  outline-green-400' value={actressname} onChange={(e) => {setActressname(e.target.value)}} type="text" placeholder="Actress Name"/>
+          <input className='px-4  outline-green-400' value={releaseddate} onChange={(e) => {setReleaseddate(e.target.value)}} type="text" placeholder="Released Date"/>
+          <input className='px-4  outline-green-400' value={genrename} onChange={(e) => {setGenrename(e.target.value)}} type="text" placeholder="Genre"/>
+          <select className='px-4 outline-green-400' onChange={(e) => {setLanguage(e.target.value)}}>
+            <option value="English">English</option>
+            <option value="Tamil">Tamil</option>
+            <option value="Hindi">Hindi</option>
+            <option value="Malayalam">Malayalam</option>
+            <option value="Kannadam">Kannadam</option>
+            <option value="Telungu">Telungu</option>
+          </select>
+         <input className='px-4' type="text" value={imageurl} onChange={(e) => {setImageurl(e.target.value)}} placeholder='Enter url of image' />
         </div>
-        <button onClick={addSong} className='bg-blue-400 text-white py-2 px-5 rounded-sm'type='submit'>Submit</button>
+        <button onClick={addSong} className='bg-blue-400 text-white py-1 px-5 rounded-sm'type='submit'>Submit</button>
       </form>
     </div>
   )
